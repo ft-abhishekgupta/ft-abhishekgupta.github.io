90a03e482e89d4db7ae2141ee3294b94a0f5a8f7
Abhishek Gupta <ft.abhishekgupta@gmail.com>
2024-04-21 14:59:25 +0530

    Update Config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,12 @@
 module.exports = {
   output: "export",
   images: { unoptimized: true },
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.igdb.com",
         port: "",
-        pathname: "",
       },
     ],
   },
